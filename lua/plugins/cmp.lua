ce28ed9c49774f81301656b1cc13eb146b427c97
Grabemonster <example@example.net>
2025-04-23 13:47:25 +0200

    cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,72 @@
+return {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for file system paths
+        {
+            "L3MON4D3/LuaSnip",
+            -- follow latest release.
+            version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+            -- install jsregexp (optional!).
+            build = "make install_jsregexp",
+        },
+        "saadparwaiz1/cmp_luasnip", -- for autocompletion
+        "rafamadriz/friendly-snippets", -- useful snippets
+        "onsails/lspkind.nvim", -- vs-code like pictograms
+    },
+
+    config = function()
+        local cmp = require("cmp")
+
+        local luasnip = require("luasnip")
+
+        local lspkind = require("lspkind")
+
+        -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+            completion = {
+                completeopt = "menu,menuone,preview,noselect",
+            },
+            snippet = { -- configure how nvim-cmp interacts with snippet engine
+                expand = function(args)
+                    luasnip.lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<CR>"] = LazyVim.cmp.confirm({ select = auto_select }),
+                ["<C-y>"] = LazyVim.cmp.confirm({ select = true }),
+                ["<S-CR>"] = LazyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<C-CR>"] = function(fallback)
+                    cmp.abort()
+                    fallback()
+                end,
+                ["<tab>"] = function(fallback)
+                    return LazyVim.cmp.map({ "snippet_forward", "ai_accept" }, fallback)()
+                end,            }),
+            -- sources for autocompletion
+            sources = cmp.config.sources({
+                { name = "nvim_lsp"},
+                { name = "luasnip" }, -- snippets
+                { name = "buffer" }, -- text within current buffer
+                { name = "path" }, -- file system paths
+            }),
+
+            -- configure lspkind for vs-code like pictograms in completion menu
+            formatting = {
+                format = lspkind.cmp_format({
+                    maxwidth = 50,
+                    ellipsis_char = "...",
+                }),
+            },
+        })
+    end,  main = "lazyvim.util.cmp",
+}
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,19 +1,4 @@
 return {
-  -- the colorscheme should be available when starting Neovim 
-  {
-    "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-    },
-    config = function()
-      -- ...
-    end,
-  },
 
   -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
   -- So for api plugins like devicons, we can always set lazy=true
@@ -21,20 +6,5 @@ return {
 
   -- you can use the VeryLazy event for things that can
   -- load later and are not important for the initial UI
-  { "stevearc/dressing.nvim", event = "VeryLazy" },
-
-  {
-    "Wansmer/treesj",
-    keys = {
-      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-    },
-    opts = { use_default_keymaps = false, max_jon_length = 150 },
-  },
-
-  {
-    "monaqa/dial.nvim",
-    -- lazy-load on keys
-    -- mode is `n` by default. For more advanced options, check the section on key mappings
-    keys = { "<C-a>", { "<C-x>", mode = "n" } },
-  },
+  { "stevearc/dressing.nvim", event = "VeryLazy" }, 
 }
