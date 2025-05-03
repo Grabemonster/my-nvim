cc69cdb243aa6e34fe0f1d07625bc7035aa84c63
Grabemonster <example@example.net>
2025-05-03 15:43:28 +0200

    nix generate lsps

--- modules/lspGen.nix
+++ modules/lspGen.nix
@@ -1,23 +1,17 @@
 { pkgs, ... }:
 
 let
-  lspServers = [
-    "lua-language-server"
-    "rust-analyzer"
-    "pyright"
+  lspList = [
+  { name = "lua-language-server"; lspconfigName = "lua_ls"; }
+  { name = "rust-analyzer";       lspconfigName = "rust_analyzer"; }
+  { name = "pyright";             lspconfigName = "pyright"; }
   ];
 
-  lspNameMap = {
-    "lua-language-server" = "lua_ls";
-    "rust-analyzer" = "rust_analyzer";
-    "pyright" = "pyright";
-  };
-
   lspConfigTemplate = lsp: ''
-    require("lspconfig").${lspNameMap.${lsp}}.setup({
-      cmd = { "${pkgs.${lsp}}/bin/${lsp}" },
-    })
-  '';
+  require("lspconfig").${lsp.lspconfigName}.setup({
+    cmd = { "${pkgs.${lsp.name}}/bin/${lsp.name}" },
+  })
+'';
 
   lspTexts = builtins.map lspConfigTemplate lspServers;
   configBody = builtins.concatStringsSep "\n\n" lspTexts;
