ad7afbdee34b275badc88c9e8651227a355dcdf0
Grabemonster <example@example.net>
2025-05-03 19:29:57 +0200

    nix generate lsps

--- modules/lspGen.nix
+++ modules/lspGen.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }:
 let
     lspList = [
-        { name = "lua-language-server"; lspconfigName = "lua_ls";}
-        { name = "pyright";             lspconfigName = "pyright";}
-        { name = "nixd";                lspconfigName = "nixd";}
+        { name = "lua-language-server";         lspconfigName = "lua_ls";}
+        { name = "pyright";                     lspconfigName = "pyright";}
+        { name = "nixd";                        lspconfigName = "nixd";}
+        { name = "bash-language-server";        lspconfigName = "bashls";}
+        { name = "typescript-language-server";  lspconfigName = "ts_ls";}
+        { name = "vscode-langservers-extracted";lspconfigName = "cssls";}
+        { name = "vscode-langservers-extracted";lspconfigName = "html";}
+        { name = "vscode-langservers-extracted";lspconfigName = "jsonls";}
+        { name = "vscode-langservers-extracted";lspconfigName = "eslint";}
     ];
 
     lspConfigTemplate = lsp: ''
@@ -12,57 +18,54 @@ let
         })
     '';
 
-    configBody = builtins.concatStringsSep "\n\n" (builtins.map lspConfigTemplate lspList);
+    configBody = builtins.concatStringsSep "\n" (builtins.map lspConfigTemplate lspList);
 
     resultText = ''
     return {
-      "neovim/nvim-lspconfig",
-       config = function()
+        "neovim/nvim-lspconfig",
+        config = function()
             local function get_cmd_for_lsp(lspconfigName)
-            -- Erstelle den Pfad zur lsp-Konfigurationsdatei für das angegebene lspconfigName
-            local lsp_file_path = "~/.local/share/nvim/lazy/nvim-lspconfig/lsp/" .. lspconfigName .. ".lua"
+                -- Erstelle den Pfad zur lsp-Konfigurationsdatei für das angegebene lspconfigName
+                local lsp_file_path = "~/.local/share/nvim/lazy/nvim-lspconfig/lsp/" .. lspconfigName .. ".lua"
 
-            -- Führe den Befehl aus, um den cmd-Wert zu extrahieren
-            local cmd_output = vim.fn.system(
-                "cat " .. lsp_file_path ..
-                " | ${pkgs.gnugrep}/bin/grep 'cmd = { '" ..
-                " | ${pkgs.coreutils}/bin/cut -f 5- -d ' '" ..
-                " | ${pkgs.gnused}/bin/sed -E 's/.*cmd = (.*)/\\1/'" ..
-                " | ${pkgs.perl}/bin/perl -pe 's/^\\{\\s*(.*?)\\s*\\},?$/\\1/' " ..
-                " | ${pkgs.gnused}/bin/sed -E \"s/'/ /g\"" ..
-                " | ${pkgs.coreutils}/bin/tr -d '[:space:]'"
-            ) 
+                -- Führe den Befehl aus, um den cmd-Wert zu extrahieren
+                local cmd_output = vim.fn.system(
+                    "cat " .. lsp_file_path ..
+                    " | ${pkgs.gnugrep}/bin/grep 'cmd = { '" ..
+                    " | ${pkgs.coreutils}/bin/cut -f 5- -d ' '" ..
+                    " | ${pkgs.gnused}/bin/sed -E 's/.*cmd = (.*)/\\1/'" ..
+                    " | ${pkgs.perl}/bin/perl -pe 's/^\\{\\s*(.*?)\\s*\\},?$/\\1/' " ..
+                    " | ${pkgs.gnused}/bin/sed -E \"s/'/ /g\"" ..
+                    " | ${pkgs.coreutils}/bin/tr -d '[:space:]'"
+                ) 
 
-            -- Entferne führende und abschließende Leerzeichen oder Zeilenumbrüche und gebe den Wert als Array zurück
-            local cmd_parts = vim.split(vim.fn.trim(cmd_output), ",")
-      
-            -- Rückgabe als Array
-            return cmd_parts
-      end
-      
-      -- Funktion zum Erstellen des vollständigen cmd-Arrays
-      local function create_cmd(base_cmd, lspconfig_name)
-        local cmd_parts = {}
-    
-        -- Holen des Array aus get_cmd_for_lsp
-        local lsp_cmd_parts = get_cmd_for_lsp(lspconfig_name)
-    
-        -- Füge den Basisbefehl zum ersten Element hinzu
-        for i, part in ipairs(lsp_cmd_parts) do
-            if i == 1 then
-                table.insert(cmd_parts, base_cmd .. part)
-            else
-                table.insert(cmd_parts, part)
+                -- Entferne führende und abschließende Leerzeichen oder Zeilenumbrüche und gebe den Wert als Array zurück
+                local cmd_parts = vim.split(vim.fn.trim(cmd_output), ",")
+
+                -- Rückgabe als Array
+                return cmd_parts
             end
-        end
+
+            -- Funktion zum Erstellen des vollständigen cmd-Arrays
+            local function create_cmd(base_cmd, lspconfig_name)
+                local cmd_parts = {}
+
+                -- Holen des Array aus get_cmd_for_lsp
+                local lsp_cmd_parts = get_cmd_for_lsp(lspconfig_name)
     
-        return cmd_parts
-      end
-      
-                
+                -- Füge den Basisbefehl zum ersten Element hinzu
+                for i, part in ipairs(lsp_cmd_parts) do
+                    if i == 1 then
+                        table.insert(cmd_parts, base_cmd .. part)
+                    else
+                        table.insert(cmd_parts, part)
+                    end
+                end
 
+                return cmd_parts
+            end
 
-    ${configBody}
+        ${configBody}
 
       end,
     }
