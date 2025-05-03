ca80eee50bbc1176b088329323899fe5d36cde36
Grabemonster <example@example.net>
2025-05-03 15:00:09 +0200

    nix generate lsps

--- modules/lspGen.nix
+++ modules/lspGen.nix
@@ -1,27 +1,26 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 let
-    lspServers = [
-        "lua-language-server"
-        "rust-analyzer"
-        "pyright"
-    ]; 
-    lspNameMap = {
-        "lua-language-server" = "lua_ls";
-        "rust-analyzer" = "rust_analyzer";
-        "pyright" = "pyright";
-    };
+  lspServers = [
+    "lua-language-server"
+    "rust-analyzer"
+    "pyright"
+  ]; 
 
-    lspConfigTemplate = lspName: ''
+  lspNameMap = {
+    "lua-language-server" = "lua_ls";
+    "rust-analyzer" = "rust_analyzer";
+    "pyright" = "pyright";
+  };
+
+  lspConfigTemplate = lspName: ''
     return {
       cmd = { "${pkgs.${lspName}}/bin/${lspName}" },
     }
-    '';
+  '';
 
-    lspConfigs = pkgs.lib.mapAttrs' (nixName: luaName:
-        {
-        lspConfigTemplate: nixName;
-        }
-    ) (pkgs.lib.filterAttrs (k: _: builtins.elem k lspServers) lspNameMap);
+  lspConfigs = pkgs.lib.mapAttrs' (nixName: luaName: 
+    lspConfigTemplate nixName
+  ) (pkgs.lib.filterAttrs (k: _: builtins.elem k lspServers) lspNameMap);
+
 in
-    lspConfigs
-
+  lspConfigs
