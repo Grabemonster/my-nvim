93252c710a2b1d2ebea825551748465c0050ed96
Grabemonster <example@example.net>
2025-05-04 20:35:20 +0200

    shortcuts

--- lua/plugins/lsp-setings.lua
+++ lua/plugins/lsp-setings.lua
@@ -1,19 +1,7 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconf = require('lspconfig') 
-        local on_attach = function(_, bufnr)
-            local opts = { buffer = bufnr, noremap = true, silent = true }
-
-            pcall(vim.keymap.del, "n", "gd", { buffer = bufnr })
-            pcall(vim.keymap.del, "n", "df", { buffer = bufnr })
-            pcall(vim.keymap.del, "n", "o", { buffer = bufnr })
-
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "<leader>df", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
-            print("LSP attact")
-        end
+        local lspconf = require('lspconfig')  
         lspconf.pyright.setup({
             on_attach = on_attach,
             settings = {
