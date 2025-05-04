3262f3ebe091b2ef247aadf6b48e4acdf2c6c7dd
Grabemonster <example@example.net>
2025-05-04 12:45:37 +0200

    lsp sources for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,6 +5,10 @@ return {
     dependencies = {
         "hrsh7th/cmp-buffer", -- source for text in buffer
         "hrsh7th/cmp-path", -- source for file system paths
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
+        "hrsh7th/cmp-cmdline",
         {
             "L3MON4D3/LuaSnip",
             -- follow latest release.
