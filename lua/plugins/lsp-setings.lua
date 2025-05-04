02b7fb5e08067e7a30c1094a538cdbb57d4c5f6a
Grabemonster <example@example.net>
2025-05-04 20:30:13 +0200

    shortcuts

--- lua/plugins/lsp-setings.lua
+++ lua/plugins/lsp-setings.lua
@@ -4,9 +4,11 @@ return {
         local lspconf = require('lspconfig') 
         local on_attach = function(_, bufnr)
             local opts = { buffer = bufnr, noremap = true, silent = true }
-            pcall(vim.keymap.del("n", "gd", { buffer = bufnr }))
-            pcall(vim.keymap.del("n", "df", { buffer = bufnr }))
-            pcall(vim.keymap.del("n", "o", { buffer = bufnr }))
+
+            pcall(vim.keymap.del, "n", "gd", { buffer = bufnr })
+            pcall(vim.keymap.del, "n", "df", { buffer = bufnr })
+            pcall(vim.keymap.del, "n", "o", { buffer = bufnr })
+
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
             vim.keymap.set("n", "<leader>df", vim.lsp.buf.hover, opts)
             vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, opts)
