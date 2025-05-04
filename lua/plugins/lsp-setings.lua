0d712aab120236b42955ba637d4a7f043370c57f
Grabemonster <example@example.net>
2025-05-04 19:42:52 +0200

    shortcuts

--- lua/plugins/lsp-setings.lua
+++ lua/plugins/lsp-setings.lua
@@ -1,7 +1,14 @@
 return {
     "neovim/nvim-lspconfig",
+    keys = {
+        {"<leader>gd", vim.lsp.buf.definition()},
+        {"<leader>of", vim.diagnostic.open_float()},
+        {"<leader>df", vim.lsp.buf.hover()},
+        {"<leader>b", "<C-o>"},
+        {"<leader>f", "<C-i>"}
+    },
     config = function()
-        local lspconf = require('lspconfig')
+        local lspconf = require('lspconfig') 
         lspconf.pyright.setup({
             settings = {
                 python = {
