240dd1e5a780df37b65e1959422f88b50290fc32
Grabemonster <example@example.net>
2025-05-13 14:48:56 +0200

    aiÄ

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,11 +1,8 @@
 return {
-    --[["Exafunction/windsurf.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        require("codeium").setup({})
-    end]]
-
+    {
+      "supermaven-inc/supermaven-nvim",
+      config = function()
+        require("supermaven-nvim").setup({})
+      end,
+    }
 }
