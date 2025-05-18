6421c70a70f6c249689451fe8624069967ed72cc
Grabemonster <example@example.net>
2025-05-18 16:31:54 +0200

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -7,6 +7,20 @@ return {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+        config = function()
+            vim.g.mkdp_auto_start = 1
+            vim.g.mkdp_auto_close = 1
+            vim.g.mkdp_refresh_slow = 0
+            vim.g.mkdp_command_for_global = 0
+            vim.g.mkdp_open_to_the_world = 0
+            vim.g.mkdp_open_ip = ''
+            vim.g.mkdp_browser = ''
+            vim.g.mkdp_echo_preview_url = 1
+            vim.g.mkdp_browserfunc = ''
+        end,
     }
+
 }
