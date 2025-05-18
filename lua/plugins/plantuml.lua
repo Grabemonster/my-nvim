d07280d3c508fd02b9ab7034be81cc68af919646
Grabemonster <example@example.net>
2025-05-18 16:24:26 +0200

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -6,10 +6,7 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
         ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
     }
 }
