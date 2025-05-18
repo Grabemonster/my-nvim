058f10d88db7ad8285b9d900f47c20aeed9588c3
Grabemonster <example@example.net>
2025-05-18 16:12:24 +0200

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -1,15 +1,3 @@
 return {
-    "weirongxu/plantuml-previewer.vim",
-    config = function()
-        require("plantuml-previewer").setup({
-            preview_command = "open -a /Applications/PlantUML.app",
-            open_cmd = "open -a /Applications/PlantUML.app",
-            plantuml_args = {
-                "-tsvg",
-                "-pipe",
-                "-charset",
-                "utf-8",
-            },
-        })
-    end,
+    "weirongxu/plantuml-previewer.vim" 
 }
