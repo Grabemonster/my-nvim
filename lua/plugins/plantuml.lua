65a30f6e1493b6e55a931e2b092d169eb3404b2e
Grabemonster <example@example.net>
2025-05-25 11:00:48 +0200

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -12,37 +12,10 @@ return {
                     name = 'plantuml_svg',
                     renderer = {
                         type = 'command',
-                        opts = { cmd = { 'firefox' } }, -- oder 'chromium', 'xdg-open'
+                        opts = { cmd = { 'xdg-open' } }, -- oder 'chromium', 'xdg-open'
                     },
-                },
-            },
-            previewers = {
-                plantuml_svg = {
-                    -- args für SVG-Ausgabe, stdin=false bedeutet Datei-Input
-                    command = 'plantuml',
-                    args = { '-tsvg', '$INPUT_FILE', '-o', '.' },
-                    stdin = false,
-                    stdout = false,
                 },
             },
         }
     }
-
-    --{
-    --    "iamcco/markdown-preview.nvim",
-    --    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    --    build = function()
-    --        vim.fn["mkdp#util#install"]()
-    --    end,
-    --    config = function()
-    --        vim.g.mkdp_auto_start = 0
-    --        vim.g.mkdp_auto_close = 1
-    --        vim.g.mkdp_refresh_slow = 0
-    --        vim.g.mkdp_command_for_global = 0
-    --        vim.g.mkdp_open_to_the_world = 0
-    --        vim.g.mkdp_browser = 'firefox'
-    --        vim.g.mkdp_echo_preview_url = 1
-    --        vim.g.mkdp_browserfunc = ''
-    --    end,
-    --}
 }
