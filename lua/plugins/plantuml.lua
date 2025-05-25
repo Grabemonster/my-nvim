529f7a94e0d8282d13ead16f770fa0506da4ca5c
Grabemonster <example@example.net>
2025-05-25 10:32:55 +0200

    markdown

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -1,19 +1,22 @@
 return { 
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-        config = function()
-            vim.g.mkdp_auto_start = 0
-            vim.g.mkdp_auto_close = 1
-            vim.g.mkdp_refresh_slow = 0
-            vim.g.mkdp_command_for_global = 0
-            vim.g.mkdp_open_to_the_world = 0
-            vim.g.mkdp_browser = 'firefox'
-            vim.g.mkdp_echo_preview_url = 1
-            vim.g.mkdp_browserfunc = ''
-        end,
+        "brianhuster/live-preview.nvim",
     }
+    --{
+    --    "iamcco/markdown-preview.nvim",
+    --    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    --    build = function()
+    --        vim.fn["mkdp#util#install"]()
+    --    end,
+    --    config = function()
+    --        vim.g.mkdp_auto_start = 0
+    --        vim.g.mkdp_auto_close = 1
+    --        vim.g.mkdp_refresh_slow = 0
+    --        vim.g.mkdp_command_for_global = 0
+    --        vim.g.mkdp_open_to_the_world = 0
+    --        vim.g.mkdp_browser = 'firefox'
+    --        vim.g.mkdp_echo_preview_url = 1
+    --        vim.g.mkdp_browserfunc = ''
+    --    end,
+    --}
 }
