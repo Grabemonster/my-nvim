3f930cb3b20d978488e05ebf48ae132ecb8aa8ce
Grabemonster <example@example.net>
2025-05-25 11:48:31 +0200

    plantuml

--- flake.nix
+++ flake.nix
@@ -25,8 +25,7 @@
                 cargo
                 lua-language-server
                 temurin-jre-bin-17
-                plantuml
-                glib
+                plantuml 
             ];
         my-nvim = pkgs.writeShellApplication {
             name = "my-nvim";
