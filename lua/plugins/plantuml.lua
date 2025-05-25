1f5b97624e6dcde0be484ec664b3a3fad99df5eb
Grabemonster <example@example.net>
2025-05-25 13:16:31 +0200

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -18,26 +18,30 @@ return {
     --    }
     --}
     { 
-    'javiorfo/nvim-soil',
+        'javiorfo/nvim-soil',
 
-    -- Optional for puml syntax highlighting:
-    dependencies = { 'javiorfo/nvim-nyctophilia' },
+        -- Optional for puml syntax highlighting:
+        dependencies = { 'javiorfo/nvim-nyctophilia' },
 
-    lazy = true,
-    ft = "plantuml",
-    opts = {
-        -- If you want to change default configurations
+        lazy = true,
+        ft = "plantuml",
+        opts = {
+            -- If you want to change default configurations
 
-        -- This option closes the image viewer and reopen the image generated
-        -- When true this offers some kind of online updating (like plantuml web server)
-        actions = {
-            redraw = true
-        },        
-        -- If you want to customize the image showed when running this plugin
-        image = {
-            darkmode = false, -- Enable or disable darkmode
-            format = "svg", -- Choose between png or svg 
+            -- This option closes the image viewer and reopen the image generated
+            -- When true this offers some kind of online updating (like plantuml web server)
+            actions = {
+                redraw = true
+            },
+            -- If you want to customize the image showed when running this plugin
+            image = {
+                darkmode = false, -- Enable or disable darkmode
+                format = "svg", -- Choose between png or svg 
+
+                execute_to_open = function(img)
+                    return
+                end
+            }
         }
     }
-}
 }
