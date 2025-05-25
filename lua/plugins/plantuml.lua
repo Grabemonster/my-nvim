5c252a9a834522144b56ea0f4d29af8fc9abb3c6
Grabemonster <example@example.net>
2025-05-25 13:43:04 +0200

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -17,31 +17,56 @@ return {
     --        },
     --    }
     --}
-    { 
-        'javiorfo/nvim-soil',
+    {
+  "plantuml-autowatch",
+  ft = { "puml", "plantuml" },
+  config = function()
+    -- Konfigurierbare Optionen
+    local plantuml_args = { "-tsvg", "-darkmode" }
 
-        -- Optional for puml syntax highlighting:
-        dependencies = { 'javiorfo/nvim-nyctophilia' },
+    -- OPTIONAL: Hier definierst du, was passieren soll, wenn Rendering abgeschlossen ist
+    local function on_rendered(output_file)
+    end
 
-        lazy = true,
-        ft = "plantuml",
-        opts = {
-            -- If you want to change default configurations
+    -- Hilfsfunktion zum Ermitteln der erwarteten Ausgabedatei (basierend auf -tXXX)
+    local function get_output_file(input_file)
+      local ext = "png" -- Default
+      for _, arg in ipairs(plantuml_args) do
+        if arg == "-tsvg" then ext = "svg" end
+        if arg == "-tpng" then ext = "png" end
+        if arg == "-teps" then ext = "eps" end
+        if arg == "-tpdf" then ext = "pdf" end
+        if arg == "-latex" then ext = "tex" end
+      end
+      return input_file:gsub("%.pu?ml$", "." .. ext)
+    end
 
-            -- This option closes the image viewer and reopen the image generated
-            -- When true this offers some kind of online updating (like plantuml web server)
-            actions = {
-                redraw = true
-            },
-            -- If you want to customize the image showed when running this plugin
-            image = {
-                darkmode = false, -- Enable or disable darkmode
-                format = "svg", -- Choose between png or svg 
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = { "*.puml", "*.plantuml" },
+      callback = function(args)
+        local file = args.file
+        local cmd = { "plantuml" }
+        vim.list_extend(cmd, plantuml_args)
+        table.insert(cmd, file)
 
-                execute_to_open = function(img)
-                    return "firefox " .. img
-                end
-            }
-        }
+        local output_file = get_output_file(file)
+
+        vim.fn.jobstart(cmd, {
+          stdout_buffered = true,
+          stderr_buffered = true,
+          on_stdout = function(_, data)
+            if data then print(table.concat(data, "\n")) end
+          end,
+          on_stderr = function(_, data)
+            if data then vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR) end
+          end,
+          on_exit = function()
+            on_rendered(output_file)
+          end,
+        })
+      end,
+    })
+  end,
+} end,
     }
 }
