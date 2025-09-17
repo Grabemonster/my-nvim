9d253c619d02512cad8bb05eb0488dba67378e7f
Grabemonster <example@example.net>
2025-09-17 18:15:56 +0200

    latex

--- modules/lspGen.nix
+++ modules/lspGen.nix
@@ -14,6 +14,7 @@ let
         { name = "jdt-language-server";        lspconfigName = "jdtls";} 
         { name = "clang-tools";                 lspconfigName = "clangd";}
         { name = "sqls";                        lspconfigName = "sqlls";}
+        { name = "texlab";                      lspconfigName = "texlab";}
 
     ];
 
